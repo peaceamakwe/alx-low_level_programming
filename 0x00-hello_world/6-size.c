104d495ab92439adaa76f22297a0499b56cb0495
peaceamakwe <pizyamakwe@gmail.com>
2023-04-21 01:44:35 -0700

    Size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("Size of a charr: %i byte(s)\n", sizeof(charr));
+	printf("size of a long int: %i byte(s)\n" size of(float));
+	return (0);
+}
