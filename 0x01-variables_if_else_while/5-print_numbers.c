c55bed7433122c4376b7874553aa3b828809abd1
peaceamakwe <pizyamakwe@gmail.com>
2023-04-23 20:00:20 -0700

    Print Nos

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int integer;
+
+	for (integer = 0; integer < 10; integer++)
+		printf("%d", integer);
+
+	printf("\n");
+
+	return (0);
+}
