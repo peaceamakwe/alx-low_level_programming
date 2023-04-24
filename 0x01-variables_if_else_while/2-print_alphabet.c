5f1cbae9345ec624db8651c3dc67828898b70be0
peaceamakwe <pizyamakwe@gmail.com>
2023-04-23 19:23:44 -0700

    Print Alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+}
+	char mn;
+
+	for (mn = 'a'; mn <= 'z'; mn++)
+	{
+		putchar(mn);
+	}
+	putchar('\n');
+	return (0);
+}
