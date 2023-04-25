b6d3bb034c3ee5bb6670a930c61ced5bfccef5c1
peaceamakwe <pizyamakwe@gmail.com>
2023-04-24 22:49:09 -0700

    Print5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <st.dio.h>
+
 /**
  * main - Entry
  * Return: Always 0
@@ -6,5 +7,6 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
