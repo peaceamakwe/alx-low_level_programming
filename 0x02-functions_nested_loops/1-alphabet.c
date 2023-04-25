189056096d035f854facf248c6a282d2fec4acba
peaceamakwe <pizyamakwe@gmail.com>
2023-04-25 08:54:58 -0700

    Task1: Alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print abcd
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+_putchar('\n');
+}
