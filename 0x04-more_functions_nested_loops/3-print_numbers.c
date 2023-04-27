4fe6fb048c32d3cf3535e0bbc90d481a96bf1ae2
peaceamakwe <pizyamakwe@gmail.com>
2023-04-27 14:41:43 -0700

    Nos

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int lm;
+
+	for (lm = 48; lm < 58; lm++)
+	{
+		_putchar(lm);
+	}
+	_putchar('\n');
+}
