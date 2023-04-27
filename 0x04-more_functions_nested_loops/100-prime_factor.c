c4878aa422bd2bb03ab026424e5de0a8fa60dd84
peaceamakwe <pizyamakwe@gmail.com>
2023-04-27 15:22:28 -0700

    Prime 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int j, ab;
+
+	j = 612852475143;
+	for (ab = 2; ab <= n; ab++)
+	{
+		if (j % ab == 0)
+		{
+			j /= ab;
+			ab--;
+		}
+	}
+	printf("%ld\n", ab);
+	return (0);
+}
