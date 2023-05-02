a564c1e3760d1dfdf781f922fa7a2dac2db5339e
peaceamakwe <pizyamakwe@gmail.com>
2023-05-02 12:15:24 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98:Resets the value of a variable back to 98
+ * reset_to_98-Resets the value of n back to 98
  * @n: Variable to be reset
  * Return: Void
  */
