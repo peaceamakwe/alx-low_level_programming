0ee0fe65ab54d65f8337a7b889038247ae17e36a
peaceamakwe <pizyamakwe@gmail.com>
2023-05-07 13:11:09 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,18 +4,18 @@
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
- * Return: s1[i] - s2[i]
+ * Return: s1x] - s2[x]
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int x;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	x = 0;
+	while (s1[x] != '\0' && s2[x] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[x] != s2[x])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[x] - s2[x]);
 		}
 		x++;
 	}
