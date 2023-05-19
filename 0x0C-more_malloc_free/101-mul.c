6d279874b7949606d6646ff77e726342de2960c2
peaceamakwe <pizyamakwe@gmail.com>
2023-05-18 21:43:04 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
-	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
